5a095b1fba61b0b9dd436c0c3b1d0004bf22e863
Gonzalo <gonzalo_delrio@live.com.ar>
2021-10-28 20:40:22 -0300

    lo dejo un poco funcional

--- src/main/java/com/helpfull/egg/controllers/MainController.java
+++ src/main/java/com/helpfull/egg/controllers/MainController.java
@@ -1,5 +1,31 @@
 package com.helpfull.egg.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class MainController {
 
+	@GetMapping("")
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/seleccion")
+	public String seleccion() {
+		return "seleccion";
+	}
+	
+	@GetMapping("/registroAmigo")
+	public String registroAmigo() {
+		return "registroAmigo";
+	}
+	
+	@GetMapping("/registroVoluntario")
+	public String registroVoluntario() {
+		return "registroVoluntario";
+	}
+	
 }
