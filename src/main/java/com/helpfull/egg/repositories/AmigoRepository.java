d4ab94f4d47e7899b9358e849d008c8df3e7518f
Gonzalo <gonzalo_delrio@live.com.ar>
2021-11-03 19:43:54 -0300

    resubida

--- src/main/java/com/helpfull/egg/repositories/AmigoRepository.java
+++ src/main/java/com/helpfull/egg/repositories/AmigoRepository.java
@@ -1,11 +1,18 @@
 package com.helpfull.egg.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.helpfull.egg.entities.Amigo;
 
 @Repository
 public interface AmigoRepository extends JpaRepository<Amigo,String>{
+	
+	@Query("SELECT a FROM Amigo a WHERE a.zona = :idZona")
+	public List<Amigo> buscarAmigosPorZona(@Param("idZona") String idZona);
 	
 }
