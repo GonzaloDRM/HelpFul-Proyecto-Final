dc4b5480cd54e91fc65b2ee5b6f7017f98eb1e78
Gonzalo <gonzalo_delrio@live.com.ar>
2021-11-16 19:24:15 -0300

    ultimo

--- src/main/java/com/helpfull/egg/controllers/MatchController.java
+++ src/main/java/com/helpfull/egg/controllers/MatchController.java
@@ -2,7 +2,6 @@ package com.helpfull.egg.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +19,7 @@ public class MatchController {
 	private MatchService matchService;
 	
 	@GetMapping("/match")
-	public String match(Model model) {
+	public String match() {
 		
 		
 		return "match";
